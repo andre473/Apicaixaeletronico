5820b7ff3860d69415f4bc76d3c48af38fa34846
andre473 <andre_02silva@hotmail.com>
2022-01-25 23:31:54 -0300

    alteração de class

--- src/main/java/com/example/apicaixaeletronico/repositories/CaixaEletronicoRepository.java
+++ src/main/java/com/example/apicaixaeletronico/repositories/CaixaEletronicoRepository.java
@@ -1,12 +1,11 @@
 package com.example.apicaixaeletronico.repositories;
 
 import com.example.apicaixaeletronico.models.CaixaEletronico;
-import com.example.apicaixaeletronico.models.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CaixaEletronicoRepository extends JpaRepository<CaixaEletronico,Long> {
+public interface CaixaEletronicoRepository extends JpaRepository<CaixaEletronico, Long> {
 
 
 }
